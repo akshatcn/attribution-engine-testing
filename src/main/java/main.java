cecf31f5e86d898fae1fcf02536657830bb801f8
Akshat Goyal <akshat.goyal@codenation.co.in>
2017-11-14 09:49:05 +0530

    Base line commit for AE analysis

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,92 +9,78 @@ public class main {
         System.out.println("In the main function");
     }
 
-    /* this is just a test comment
-
-	which continues here. */
-    public void longMethod(){
-
-        int abc=0; int def=1000;
-
-        int a = 10;
-        int b = 5;
-        System.out.println("123");
-        System.out.println("123");
-        float d = 2;
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        float c = a/(float)b;
-        System.out.println(c);
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-	//This is another comment
-        System.out.println("123");
-        if(d == c){
-            int myInt = 0;
-        } else {
-            int myInt = 1;
+    public static void func1(Integer i){
+        //move the following lines to last
+        System.out.println("This is the first line. Which would be moved to last");
+        if(i > 0){
+            System.out.println("This is inside a block");
         }
 
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-	//Just adding another random comment
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
+        int x = i;
+        int modifyVariableName = 0;
+        double modifyVariableType = 0.0;
+        int changeVariableNameAndType = 0;
 
-        for(int i = 0; i<a; i++){
-            System.out.print("Doing Nothing");
+        int a = 1;
+        int b = 1;
+        int c = 1;
+
+        int modifyThisExpression = (a+3)/(b-10)*(c+1);
+        int changeThisExpression = (a+3)/(b-10)*(c+1);
+        int changeVariableNames = (a+3)/(b-10)*(c+1);
+
+        System.out.println("Modify this line");
+        System.out.println(x);
+
+
+        //Swap the following lines
+        System.out.println("First");
+        System.out.println("Second");
+
+
+        System.out.println("1");
+        String move = "This is the line that would be moved";
+
+        System.out.println("2");
+        String moveInBlock = "This is the line that would be moved into the block";
+
+        System.out.println("3");
+        for(int z=0; z<10; z--) {
+            x++;
         }
 
-        List list = new ArrayList<Integer>();
+        System.out.println("4");
 
-        int x = 0;
+        System.out.println("5");
 
-        for(int i = 0; i < 10; ){
-            i--;
+        System.out.println("6");
+        //remove the line from inside the for loop initialisation
+        for(int init=0; init<10; init--) {
+            x++;
         }
 
+        System.out.println("7");
 
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
+        System.out.println("8");
+        System.out.println("Move this line to different function");
     }
 
-    public void longMethod2(){
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
+    void func2(String s){
+        int addAnotherVarAfterThis = 0;
+
+        System.out.println("This is the static line");
+
+        int changeVariableNameAfterMoving = 99;
+        int changeValueAfterMoving = 99;
+        System.out.println("This is another base line");
+
+
+    }
+
+    void func3(Double d){
+        int deleteThisVar = 1111;
+        System.out.println("This is base for this function");
+
+
     }
 }
